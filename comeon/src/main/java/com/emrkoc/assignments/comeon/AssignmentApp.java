562f7fd9ed534db23843c9e5f6cc7e638367c352
Emrullah Koçmar <emokocmar@gmail.com>
2019-02-16 22:42:29 +0300

    Assignment: initial commit
    - pom created for spring boot
    - main app is added
    - application properties added

---
+++ comeon/src/main/java/com/emrkoc/assignments/comeon/AssignmentApp.java
@@ -0,0 +1,11 @@
+package com.emrkoc.assignments.comeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentApp.class);
+    }
+}
